5ced2ff84fe8c25ee9f3324454cc2af6a9d1c50b
James Munns <james.munns@gmail.com>
2017-03-22 03:07:55 +0100

    Ugly, but works :)

--- pide-rs/src/cli.rs
+++ pide-rs/src/cli.rs
@@ -6,6 +6,7 @@ use std::process::Command;
 use toml;
 use std::io::prelude::*;
 use std::fs::File;
+use std::env::current_dir;
 
 
 
@@ -14,20 +15,17 @@ pub fn parse_cli() {
         .author(crate_authors!())
         .version(crate_version!())
 
-        // "zeit init"
+        // "pide init <dockerfile>"
         .subcommand(SubCommand::with_name("init")
             .about("Initialize pide here")
             .arg(Arg::with_name("Dockerfile")
                 .help("Dockerfile to initialize this environment with")
                 .required(true)))
 
-        // // "zeit pom"
-        // .subcommand(SubCommand::with_name("pom")
-        //     .about("Start, end, or modify a pomodoro time")
+        // "pide resume"
+        .subcommand(SubCommand::with_name("resume")
+            .about("Resume an existing pide"))
 
-        //     // "zeit pom start"
-        //     .subcommand(SubCommand::with_name("start")
-        //         .help("start a new pomodoro session")))
         .get_matches();
 
     match matches.subcommand() {
@@ -35,20 +33,75 @@ pub fn parse_cli() {
             let dockerfile = args.value_of("Dockerfile").expect("Missing required argument?");
             init(dockerfile);
         }
-        //     ("pom", Some(x)) => {
-        //         pomodoro(x);
-        //     }
+        ("resume", _) => {
+            resume();
+        }
         _ => println!("Hallo, welt!"),
     }
 }
 
+fn resume() {
+    // Load pide file
+    let pide_data = PideFile::from_file(".pide");
+    assert_eq!(pide_data.pide_version, "0.2.0", "Version Mismatch!");
+
+    let output = Command::new("docker")
+        .arg("images")
+        .output()
+        .expect("Failed to get existing images")
+        .stdout;
+
+    let existing_images = String::from_utf8_lossy(&output);
+
+    let dockerfile = pide_data.original_dockerfile;
+    let id: String = pide_data.name.id.to_string();
+    let name_str = pide_data.name.to_string();
+
+    let name = if !existing_images.contains(&id) {
+        println!("Running for the first time...");
+        &pide_data.image_id
+    } else {
+        println!("Resuming...");
+        &name_str
+    };
+
+    let temp_name = Uuid::new_v4().to_string();
+
+    let process = Command::new("docker")
+        .arg("run")
+        .arg("-v")
+        .arg(format!("{}:/host",
+                     current_dir()
+                         .expect("failed to get cwd")
+                         .to_str()
+                         .expect("bad utf-8 in cwd")))
+        .arg("--name")
+        .arg(&temp_name)
+        .arg("-it")
+        .arg(&name)
+        .arg("/bin/bash")
+        .spawn()
+        .expect("Docker failed to launch")
+        .wait()
+        .expect("Docker failed at runtime");
+
+    println!("Committing container history...");
+    let _ = Command::new("docker")
+        .arg("commit")
+        .arg(&temp_name)
+        .arg(&name_str)
+        .output()
+        .expect("Failed to commit docker changes");
+
+}
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
-pub struct Pidename {
+pub struct PideName {
     repo: String,
     id: Uuid,
 }
 
-impl Pidename {
+impl PideName {
     pub fn new() -> Self {
         Self {
             repo: "pide".to_string(),
@@ -62,17 +115,17 @@ impl Pidename {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct Pidefile {
+pub struct PideFile {
     image_id: String,
     original_dockerfile: String, // TODO: not a string?
     original_workingpath: String, // TODO: not a string?
     pide_version: String, // TODO: not a string?
 
     // Tables go last
-    name: Pidename,
+    name: PideName,
 }
 
-impl Pidefile {
+impl PideFile {
     pub fn to_string(&self) -> Result<String, ()> {
         match toml::to_string(self) {
             Ok(out) => Ok(out),
@@ -82,12 +135,21 @@ impl Pidefile {
             }
         }
     }
+
+    pub fn from_file(open_path: &str) -> Self {
+
+        let mut file = File::open(open_path)
+            .expect(&format!("Failed to open pide file {:}", open_path));
+        let mut contents = String::new();
+        let _ = file.read_to_string(&mut contents);
+        toml::from_str(&contents).expect("yiss")
+    }
 }
 
 fn init(dockerfile: &str) {
     // println!("{}", dockerfile);
 
-    // println!("{:?}", Pidename::new().to_string());
+    // println!("{:?}", PideName::new().to_string());
     let dfp = Path::new(dockerfile);
     let working_path = dfp.parent().expect("Failed to choose working path");
 
@@ -111,8 +173,8 @@ fn init(dockerfile: &str) {
         .expect("Failed to decode image id")
         .to_string();
 
-    let pide_data = Pidefile {
-        name: Pidename::new(),
+    let pide_data = PideFile {
+        name: PideName::new(),
         image_id: image_id,
         original_dockerfile: dockerfile.to_string(),
         original_workingpath: working_path.to_str().unwrap().to_string(),
